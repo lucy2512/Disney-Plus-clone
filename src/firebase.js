4c5b9ae555d8260c611db59163fbfa0f05e80a9b
lucy2512 <mail4parthib@gmail.com>
2021-05-10 22:06:00 +0530

    Firebase half done

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAL2mYWNYeQpmdIuCPlYbC_r9a9dA5gndU",
+    authDomain: "disney-plus-clone-52fc0.firebaseapp.com",
+    projectId: "disney-plus-clone-52fc0",
+    storageBucket: "disney-plus-clone-52fc0.appspot.com",
+    messagingSenderId: "213050555070",
+    appId: "1:213050555070:web:48ec16181f9aaabe293cef",
+    measurementId: "G-T2Q77CTSP7"
+  };
+
+  
