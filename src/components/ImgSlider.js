270fd2afb32445e8531544d6fc10ab5d2a53bf3a
lucy2512 <mail4parthib@gmail.com>
2021-06-06 19:57:59 +0530

    :art:ImgSlider Style Added

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -14,15 +14,61 @@ const ImgSlider = (props) => {
         autoplay: true,
     }
     return(
-        <Slider {...settings}>
+        <Carosuel {...settings}>
             <div>
                 <h3>1</h3>
             </div>
             <div>
                 <h3>2</h3>
             </div>
-        </Slider>
+        </Carosuel>
     )
 }
+
+const Carosuel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+      opacity:0;
+      height: 100%;
+      width: 5vh;
+      z-index: 1;
+  
+
+  &:hover{
+      opacity:1;
+      transition: opacity 0.2s ease 0s;
+  }
+}
+
+//Dots under slider
+ul li button {
+    &:before{
+        font-size: 10px;
+        color: rgb(150,158,171);
+    }
+}
+
+li.slick-active button:before {
+    color: white;
+}
+
+//Upcoming slider
+
+.slick-list {
+    overflow: initial;
+}
+
+
+//Left & Right Buttons
+ .slick-prev {
+     left: -75px;
+ }
+
+ .slick-next {
+     right: -75px;
+ }
+
+`;
 
 export default ImgSlider;
