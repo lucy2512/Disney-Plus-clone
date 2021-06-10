0a37692633a822741ff60e17fdb7d0eb73293228
lucy2512 <mail4parthib@gmail.com>
2021-06-10 11:32:29 +0530

    :zap: Viewers Images Syling Done

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -35,6 +35,22 @@ const Container = styled.div`
 
 const Wrap = styled.div`
  padding-top: 56.25%;
+ border-radius: 10px;
+ box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+ cursor: pointer;
+ overflow: hidden;
+ position: relative;
+ transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+ border: 3px solid rgba(249,249,249,0.1);
+
+ img{
+     inset: 0px;
+     display: block;
+     height: 100%;
+     object-fit: cover;
+     opacity: 1;
+     position: absolute;
+ }
 `;
 
 export default Viewers;
