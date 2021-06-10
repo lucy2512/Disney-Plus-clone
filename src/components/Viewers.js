0a930bda334558747dc46c481f93ccb600546bba
lucy2512 <mail4parthib@gmail.com>
2021-06-10 11:43:04 +0530

    :boom: Viewers Videos Added

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -4,18 +4,33 @@ const Viewers = (props) => {
     return <Container>
         <Wrap>
             <img src="/images/viewers-disney.png" alt=""/>
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src="/videos/1564674844-disney.mp4" type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-pixar.png" alt=""/>
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src="/videos/1564676714-pixar.mp4" type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-marvel.png" alt=""/>
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src="/videos/1564676115-marvel.mp4" type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-starwars.png" alt=""/>
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src="/videos/1608229455-star-wars.mp4" type='video/mp4'/>
+            </video>
         </Wrap>
         <Wrap>
             <img src="/images/viewers-national.png" alt=""/>
+            <video autoPlay={true} loop={true} playsInline={true}>
+                <source src="/videos/1564676296-national-geographic.mp4" type='video/mp4'/>
+            </video>
         </Wrap>
     </Container>
 };
@@ -50,6 +65,15 @@ const Wrap = styled.div`
      object-fit: cover;
      opacity: 1;
      position: absolute;
+     trnsition: opacity 500ms ease-in-out 0s;
+     width: 100%;
+     z-index:1;
+     top: 0;
+ }
+
+ video{
+     width: 100%;
+     height: 100%;
  }
 `;
 
