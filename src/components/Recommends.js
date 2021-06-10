dd5cde61f630cff9b90301c825b177a71bc51b57
lucy2512 <mail4parthib@gmail.com>
2021-06-10 11:56:50 +0530

    :zap: Recommends Part Added

---
+++ src/components/Recommends.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+const Recommends = (props) => {
+    return(
+        <div>
+            <h4>Recommended for You</h4>
+        </div>
+    );
+};
+
+export default Recommends;
