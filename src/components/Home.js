9175919eb727b8db31829be134ffc609432cde40
lucy2512 <mail4parthib@gmail.com>
2021-06-11 09:10:58 +0530

    :zap:NewDisney Section Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
 import Recommends from './Recommends';
 import Viewers from './Viewers';
+import NewDisney from './NewDisney';
 
 const Home = (props) => {
     return(
@@ -9,6 +10,7 @@ const Home = (props) => {
           <ImgSlider/>
           <Viewers/>
           <Recommends/>
+          <NewDisney/>
         </Container>
     )
 }
