41004ae15451cff14db14dda0fb52cf7b438b5f2
lucy2512 <mail4parthib@gmail.com>
2021-06-13 21:13:27 +0530

    Getting Data From Firebase

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,56 @@ import Viewers from './Viewers';
 import NewDisney from './NewDisney';
 import Originals from './Originals';
 import Trendings from './Trendings';
+import {useEffect } from 'react';
+import { useDispatch, useSelector  } from 'react-redux';
+import db from '../firebase';
+import { setMovies } from '../features/movie/movieSlice';
+import { selectUserName, selectUserPhoto } from '../features/user/userSlice';
 
 const Home = (props) => {
+
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
+
+  //React Hook
+  useEffect(() => {
+    db.collection('movies').onSnapshot((snapshot) => {
+       snapshot.docs.map((doc) => {
+         console.log(recommends);
+         switch(doc.data().type) {
+           case 'recommend':
+             recommends = [...recommends, {id: doc.id, ...doc.data()}];
+             break;
+
+           case 'new': 
+           newDisneys = [...newDisneys, {id: doc.id, ...doc.data()}];
+           break;
+
+           case 'original':
+            originals = [...originals, {id: doc.id, ...doc.data()}];
+             break;
+
+           case 'trending': 
+           trending = [...trending, {id: doc.id, ...doc.data()}];
+           break;
+         }
+       });
+    });
+
+    dispatch(
+      setMovies({
+        recommend: recommends,
+        newDisney: newDisneys,
+        original: originals,
+        trending: trending,
+    })
+    );
+  }, [userName])
+
     return(
         <Container>
           <ImgSlider/>
