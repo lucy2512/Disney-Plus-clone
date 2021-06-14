a7ae15ef6c1693658bc57f20bb5f07ddf011bbf7
lucy2512 <mail4parthib@gmail.com>
2021-06-14 08:43:44 +0530

    :boom:All Imgaes Loaded

--- src/components/Recommends.js
+++ src/components/Recommends.js
@@ -42,7 +42,7 @@ const Content = styled.div`
 `;
 
 const Wrap = styled.div`
- padding-top:56.25%;
+ padding-top:50.25%;
  border-radius: 10px;
  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,rgb(0 0 0 / 73%) 0px 16px 10px -10px;
  cursor: pointer;
--- src/components/Trendings.js
+++ src/components/Trendings.js
@@ -1,34 +1,25 @@
 import styled from "styled-components";
 import { Link } from 'react-router-dom';
+import {useSelector } from 'react-redux';
+import { selectTrendings } from "../features/movie/movieSlice";
 
 const Trendings = (props) => {
+
+    const movies = useSelector(selectTrendings);
+
     return(
         <Container>
-            <h4>Trendings</h4>
+            <h4>Trending</h4>
             <Content>
-               <Wrap>
-                    <Link to='/'>
-                        <img src="" alt=""/>
-                    </Link>
-               </Wrap>
-
-               <Wrap>
-                    <Link to='/'>
-                        <img src="" alt=""/>
-                    </Link>
-               </Wrap>
-
-               <Wrap>
-                    <Link to='/'>
-                        <img src="" alt=""/>
-                    </Link>
-               </Wrap>
-
-               <Wrap>
-                    <Link to='/'>
-                        <img src="" alt=""/>
-                    </Link>
-               </Wrap>
+               {movies &&
+                movies.map((movie, key) => (
+                    <Wrap key={key}>
+                        {movie.id}
+                        <Link to={`/details/` + movie.id}>
+                            <img src={movie.cardImg} alt={movie.title} />
+                        </Link>
+                    </Wrap>
+                ))}
             </Content>
         </Container>
     );
@@ -51,7 +42,7 @@ const Content = styled.div`
 `;
 
 const Wrap = styled.div`
- padding-top:56.25%;
+ padding-top:50.25%;
  border-radius: 10px;
  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,rgb(0 0 0 / 73%) 0px 16px 10px -10px;
  cursor: pointer;
