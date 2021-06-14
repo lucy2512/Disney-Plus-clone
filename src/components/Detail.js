f3abc8abb08f561ea4f5be2916d0d3d2e15397ef
lucy2512 <mail4parthib@gmail.com>
2021-06-14 20:34:58 +0530

    :boom: Imagetitle Added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -9,6 +9,13 @@ const Detail =() => {
                     src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4E9E81584305009D6385F6178D4B6930E97CD6EC4A3B53C818400DEF778FFA9A/scale?width=1440&aspectRatio=1.78&format=jpeg"
                 />
             </Background>
+
+            <ImageTitle>
+                <img
+                alt=""
+                src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/50B933E83609BEEFEDFA177A6D96DBFA7804C14F70A0B5AB314E892E65498ACF/scale?width=1440&aspectRatio=1.78"
+                />
+            </ImageTitle>
         </Container>
     )
 }
@@ -36,6 +43,24 @@ const Background = styled.div`
         @media(max-width: 768px){
             width: initial;
         }
+    }
+`;
+
+const ImageTitle = styled.div`
+    align-items: flex-end;
+    display: flex;
+    -webkit-box-pack : start;
+    justify-content: flex-atart;
+    margin: 0px auto;
+    height: 30vw;
+    min-height: 170px;
+    padding-bottom: 24px;
+    width: 100%;
+
+    img{
+        max-width: 600px;
+        min-width: 200px;
+        width: 35vw;
     }
 `;
 
