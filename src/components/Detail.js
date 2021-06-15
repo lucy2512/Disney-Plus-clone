506791a3cb0932dcc6bbb1ccb536f4d2b6fd493a
lucy2512 <mail4parthib@gmail.com>
2021-06-15 10:48:56 +0530

    :zap:Trailer Button Added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -23,6 +23,10 @@ const Detail =() => {
                         <img src="/images/play-icon-black.png" alt=""/>
                         <span>Play</span>
                     </Player>
+                    <Trailer>
+                        <img src="/images/play-icon-white.png" alt=""/>
+                        <span>Trailer</span>
+                    </Trailer>
                 </Controls>
             </ContentMeta>
         </Container>
@@ -122,4 +126,9 @@ const Player = styled.button`
     }
 `;
 
+const Trailer = styled(Player)`
+    background: rgba(0,0,0,0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+`;
 export default Detail;
