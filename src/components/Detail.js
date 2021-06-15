52adcd400f7980b7ab215c44a998e0897c866275
lucy2512 <mail4parthib@gmail.com>
2021-06-15 11:05:34 +0530

    :zap:AddList Icon Added

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -27,6 +27,10 @@ const Detail =() => {
                         <img src="/images/play-icon-white.png" alt=""/>
                         <span>Trailer</span>
                     </Trailer>
+                    <AddList>
+                        <span />
+                        <span />
+                    </AddList>
                 </Controls>
             </ContentMeta>
         </Container>
@@ -130,5 +134,35 @@ const Trailer = styled(Player)`
     background: rgba(0,0,0,0.3);
     border: 1px solid rgb(249, 249, 249);
     color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+    margin-right: 16px;
+    height: 44px;
+    width: 44px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0,0,0,0.6);
+    border-radius: 50%;
+    border: 2px solid white;
+    cursor: pointer;
+
+    span{
+        background-color: rgb(249,249,249);
+        display: inline-block;
+
+        &:first-child {
+            height: 2px;
+            transform: translate(1px, 0px) rotate(0deg);
+            width: 16px;
+        }
+
+        &:nth-child(2){
+            height: 16px;
+            transform: translateX(-8px) rotate(0deg);
+            width:2px;
+        }
+    }
 `;
 export default Detail;
